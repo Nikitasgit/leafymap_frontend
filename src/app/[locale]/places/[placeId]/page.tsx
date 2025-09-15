e547de10bbc98fda37dc5d6d050e830048dc4373
Victor Leman <victorleman1@gmail.com>
2025-09-15 16:16:56 +0200

    fixed building errors

--- src/app/[locale]/places/[placeId]/page.tsx
+++ src/app/[locale]/places/[placeId]/page.tsx
@@ -1,34 +1,17 @@
 "use client";
-import ProfilePictureUploader from "@/components/common/inputs/profilePictureUploader";
 import LoadingBar from "@/components/common/loading/LoadingBar";
-import { useCurrentUser } from "@/hooks/useCurrentUser";
 import { usePlace } from "@/hooks/usePlace";
 import { useParams } from "next/navigation";
 import React from "react";
 
 const PlacePage = () => {
   const { placeId } = useParams();
-  const { place, isLoading: placeLoading } = usePlace(placeId as string);
-  const { user, isLoading: userLoading } = useCurrentUser();
-  const isCurrentUserPlace = user?.places?.some(
-    (place) => place._id === placeId
-  );
+  const { isLoading: placeLoading } = usePlace(placeId as string);
 
-  if (placeLoading || userLoading) return <LoadingBar />;
+  if (placeLoading) return <LoadingBar />;
   return (
-    <div style={{ padding: "2rem", maxWidth: "600px", margin: "0 auto" }}>
+    <div>
       <h1>Place Profile Picture</h1>
-      <div style={{ marginBottom: "2rem" }}>
-        <h3>Small Size Example</h3>
-        <ProfilePictureUploader
-          entityType="place"
-          entityId={placeId as string}
-          initialImage={place?.image}
-          size="medium"
-          disabled={!isCurrentUserPlace}
-          isOwner={isCurrentUserPlace}
-        />
-      </div>
     </div>
   );
 };
