803ad96cb9c3321869009a689d34ef659ef4fef5
Victor Leman <victorleman1@gmail.com>
2025-09-16 11:03:41 +0200

    remove middleware logic for token verification

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,81 +1,8 @@
 import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
 import { i18nRouter } from "next-i18n-router";
 import { i18nConfig } from "./i18nConfig";
-import { verifyTokenServer } from "./utils/tokenVerification";
 
 export async function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-  const detectedLocale = i18nConfig.locales.find((locale) =>
-    pathname.startsWith(`/${locale}/`)
-  );
-  const pathNameWithoutLocale = detectedLocale
-    ? pathname.replace(`/${detectedLocale}`, "")
-    : pathname;
-
-  const protectedRoutes = ["/account", "/messages"];
-  const isProtectedRoute = protectedRoutes.some((route) =>
-    pathNameWithoutLocale.startsWith(route)
-  );
-
-  if (isProtectedRoute) {
-    const token = request.cookies.get("token")?.value;
-    console.log("Token found, redirecting to account");
-    if (!token) {
-      console.log("No token found, redirecting to signin");
-      const signinUrl = new URL(`/auth/signin`, request.url);
-      return NextResponse.redirect(signinUrl);
-    }
-
-    const tokenVerification = await verifyTokenServer(token);
-
-    if (!tokenVerification.isValid) {
-      console.log("Token verification failed, redirecting to signin");
-      const signinUrl = new URL(`/auth/signin`, request.url);
-      return NextResponse.redirect(signinUrl);
-    }
-
-    const userType = tokenVerification.user?.user.userType;
-
-    // Guest-only routes
-    if (pathNameWithoutLocale === "/account/create" && userType !== "guest") {
-      const accountUrl = new URL(`/account`, request.url);
-      return NextResponse.redirect(accountUrl);
-    }
-
-    // Creator-only routes
-    if (
-      pathNameWithoutLocale === "/account/update-creator" &&
-      userType !== "creator"
-    ) {
-      const accountUrl = new URL(`/account`, request.url);
-      return NextResponse.redirect(accountUrl);
-    }
-
-    // Organizer-only routes
-    const organizerOnlyRoutes = ["/account/places/create"];
-
-    const isOrganizerOnlyRoute = organizerOnlyRoutes.some((route) =>
-      pathNameWithoutLocale.startsWith(route)
-    );
-
-    // Special handling for place-specific routes
-    const placeIdPattern = /^\/account\/places\/[^\/]+$/;
-    const isPlaceSpecificRoute = placeIdPattern.test(pathNameWithoutLocale);
-
-    if (isOrganizerOnlyRoute && userType !== "organizer") {
-      const accountUrl = new URL(`/account`, request.url);
-      return NextResponse.redirect(accountUrl);
-    }
-
-    // Restrict access to specific place pages to organizers only
-    if (isPlaceSpecificRoute && userType !== "organizer") {
-      const accountUrl = new URL(`/account`, request.url);
-      return NextResponse.redirect(accountUrl);
-    }
-  }
-
   const i18nResponse = i18nRouter(request, i18nConfig);
   return i18nResponse;
 }
