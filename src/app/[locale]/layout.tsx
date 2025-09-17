31a807a04b66e812c81b6480523b4e1179e58775
Victor Leman <victorleman1@gmail.com>
2025-09-17 09:24:56 +0200

    added vercel speed analytics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "@/styles/main.scss";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Navbar from "@/components/navbar/Navbar";
 import Providers from "@/components/Providers";
 
@@ -20,6 +21,7 @@ export default async function RootLayout({
       <body className={inter.className}>
         <Providers locale={locale}>
           <Analytics />
+          <SpeedInsights />
           <Navbar />
           <main data-locale={locale}>{children}</main>
         </Providers>
