d5a98e0a5fdcb8a37c679094a121d4997e919ab4
Victor Leman <victorleman1@gmail.com>
2025-09-26 18:01:44 +0200

    test metadata home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,19 @@
 import HomeHeader from "@/components/home/appPresentation";
 import SuggestionsList from "@/components/home/suggestionsList";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title:
+    "SpotLight - Découvrez les créateurs, artisans et lieux culturels près de chez vous",
+  description:
+    "SpotLight est la plateforme de mise en relation entre créateurs, artisans, responsables de lieux et passionnés d'art et de culture. Explorez une carte interactive, participez à des événements et entrez en contact avec les talents locaux.",
+};
 
 export default async function Home() {
   return (
-    <div>
+    <>
       <HomeHeader />
       <SuggestionsList />
-    </div>
+    </>
   );
 }
