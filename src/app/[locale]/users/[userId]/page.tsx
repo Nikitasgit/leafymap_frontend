a7a8f4fc55ec608f46d44bcb985cce638b6e7e1b
Victor Leman <victorleman1@gmail.com>
2025-09-29 13:43:39 +0200

    console log de l'uitlisateur en prod 2

--- src/app/[locale]/users/[userId]/page.tsx
+++ src/app/[locale]/users/[userId]/page.tsx
@@ -1,13 +1,21 @@
 import { UserProfileContainer } from "@/components/userProfile";
 import { generateUserMetadata } from "@/lib/metadata";
 
-export async function generateMetadata({ params }: { params: { userId: string } }) {
-  const { userId } = params;
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{
+    userId: string;
+  }>;
+}) {
+  const { userId } = await params;
+  console.log("userId", userId);
+
   return generateUserMetadata(userId);
 }
 
 const UserPage = () => {
-  return <UserProfileContainer   />;
+  return <UserProfileContainer />;
 };
 
 export default UserPage;
