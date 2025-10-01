1ed5a56e6c3feb7b4d1bf4f7c9279168bfedcf0b
Victor Leman <victorleman1@gmail.com>
2025-10-01 16:02:39 +0200

    improved ui if no suggestions on home page

--- src/components/home/SuggestionsList/SuggestionsList.tsx
+++ src/components/home/SuggestionsList/SuggestionsList.tsx
@@ -1,28 +1,34 @@
 "use client";
 import { useFindPlaces } from "@/hooks/useFindPlaces";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import styles from "./SuggestionsList.module.scss";
 import PlaceSuggestionCard from "../PlaceSuggestionCard";
+import EmptyState from "@/components/common/noResults/EmptyState";
+import { MapPin } from "lucide-react";
 
 const SuggestionsList = () => {
   const { searchResults, isLoading, searchPlaces } = useFindPlaces();
   useEffect(() => {
-    if (searchResults.length === 0) {
-      searchPlaces();
-    }
-  }, [searchPlaces, searchResults]);
+    searchPlaces();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <section className={styles.suggestionsList}>
       <h2>Les lieux à ne pas manquer</h2>
       {isLoading ? (
         <div className={`${styles.suggestionsListGrid} skeleton`}></div>
-      ) : (
+      ) : searchResults.length > 0 ? (
         <div className={styles.suggestionsListGrid}>
           {searchResults.map((suggestion) => (
             <PlaceSuggestionCard key={suggestion._id} place={suggestion} />
           ))}
         </div>
+      ) : (
+        <EmptyState
+          title="Aucun lieu trouvé"
+          icon={<MapPin className={styles.icon} />}
+        />
       )}
     </section>
   );
