404d8ba20aaf7178f4342890f2c6bd70a39f479b
nikitasgit <victorleman1@gmail.com>
2026-07-16 14:40:12 +0200

    fix: missing i18n keys

--- src/components/account/Place/DefaultScheduleForm/DefaultScheduleForm.tsx
+++ src/components/account/Place/DefaultScheduleForm/DefaultScheduleForm.tsx
@@ -2,6 +2,7 @@ import DayInput from "../DefaultScheduleDay";
 import styles from "./DefaultScheduleForm.module.scss";
 import { DaySchedule, WeekDay } from "@/types/place/schedule";
 import { DefaultScheduleFormProps } from "./DefaultScheduleForm.types";
+import { useTranslation } from "react-i18next";
 
 const days: WeekDay[] = [
   "monday",
@@ -17,12 +18,14 @@ const DefaultScheduleForm = ({
   schedule,
   onChange,
 }: DefaultScheduleFormProps) => {
+  const { t } = useTranslation("common");
+
   const handleDayChange = (day: WeekDay, updated: DaySchedule) => {
     onChange({ ...schedule, [day]: updated });
   };
   return (
     <fieldset className={styles.defaultScheduleForm}>
-      <legend className={styles.title}>Horaires</legend>
+      <legend className={styles.title}>{t("defaultSchedule.title")}</legend>
       {days.map((day) => (
         <DayInput
           key={day}
